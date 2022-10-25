c248d1205a2d8865537758444a9d63a8b93ded1f
Tiago Santos <tiago.dos.santos@hitachivantara.com>
2022-10-25 18:54:16 +0100

    cleanup

--- completion/sdk.completion.bash
+++ completion/sdk.completion.bash
@@ -3,5 +3,5 @@
 
 if [[ ! -f $BASH_COMPLETION_USER_DIR/completions/sdk ]] && [ "$SDKMAN_DIR" ]; then
   check_dotfile $BASH_COMPLETION_USER_DIR/completions/sdk "$SDKMAN_DIR"/contrib/completion/bash/sdk
-  echo "sdkman_auto_completE=false" > $SDKMAN_DIR/etc/config
+  echo "sdkman_auto_complete=false" > $SDKMAN_DIR/etc/config
 fi
