114a5caa85d725118543aaaa200707924d6a0439
Tiago Santos <tiago.dos.santos@hitachivantara.com>
2023-12-21 12:05:43 +0000

    update(nvim): missing telescope configs

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -26,19 +26,54 @@ return {
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
 
       vim.keymap.set('n', '<C-p>', function()
-          vim.fn.system('git rev-parse --is-inside-work-tree')
-          if vim.v.shell_error == 0 then
-              builtin.git_files({})
-          else
-              builtin.find_files({ hidden = true })
-          end
-  end, {})
+        vim.fn.system('git rev-parse --is-inside-work-tree')
+        if vim.v.shell_error == 0 then
+          builtin.git_files({})
+        else
+          builtin.find_files({ hidden = true })
+        end
+      end, {})
     end
   },
 
   -- fzf
-  { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+    config = function()
+      local fzf_opts = {
+        fuzzy = true,                   -- false will only do exact matching
+        override_generic_sorter = true, -- override the generic sorter
+        override_file_sorter = true,    -- override the file sorter
+        case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+        -- the default case_mode is "smart_case"
+      }
+      require("telescope").setup({
+        pickers = {
+          -- Manually set sorter, for some reason not picked up automatically
+          -- Ref: https://github.com/nvim-telescope/telescope.nvim/issues/2104#issuecomment-1223790155
+          lsp_dynamic_workspace_symbols = {
+            sorter = require("telescope").extensions.fzf.native_fzf_sorter(fzf_opts),
+          },
+        },
+        extensions = {
+          fzf = fzf_opts
+        },
+      })
+      require("telescope").load_extension("fzf")
+    end
+  },
 
-  { 'nvim-telescope/telescope-ui-select.nvim' },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup({
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown({})
+        }
+      })
+      require("telescope").load_extension("ui-select")
+    end
+  },
 
 }
