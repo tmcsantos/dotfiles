46430d5695c2edaa810141af9733d530fed648cb
Tiago Santos <tiago.dos.santos@hitachivantara.com>
2024-05-28 12:10:26 +0100

    nvim plugin configs

--- nvim/lua/plugins/cellular-automaton.lua
+++ nvim/lua/plugins/cellular-automaton.lua
@@ -1,6 +1,11 @@
 return {
   "eandrju/cellular-automaton.nvim",
   config = function()
-    vim.keymap.set("n", "<leader>ml", "<cmd>CellularAutomaton make_it_rain<CR>")
+    -- vim.keymap.set("n", "<leader>ml", "<cmd>CellularAutomaton make_it_rain<CR>")
+    vim.keymap.set("n", "<leader>ml", function()
+      local automaton = require("cellular-automaton")
+      local animation_list = vim.tbl_keys(automaton.animations)
+      automaton.start_animation(animation_list[math.random(#animation_list)])
+    end)
   end,
 }
