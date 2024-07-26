23b1e93900ed8bd4e625f92b35e67c2a6d134132
Tiago Santos <tiago.dos.santos@hitachivantara.com>
2024-07-26 17:10:58 +0100

    ipython config

--- ipython/ipython_config.py
+++ ipython/ipython_config.py
@@ -7,9 +7,14 @@ c.TerminalInteractiveShell.true_color = True
 # list(get_all_styles())
 c.TerminalInteractiveShell.highlighting_style = 'gruvbox-dark'
 c.TerminalInteractiveShell.editing_mode = 'vi'
+c.TerminalInteractiveShell.editor = 'vi'
+c.TerminalInteractiveShell.timeoutlen = 0
+c.TerminalInteractiveShell.true_color = True
 
 c.InteractiveShellApp.exec_lines = []
 c.InteractiveShellApp.exec_lines.append('%load_ext autoreload')
 c.InteractiveShellApp.exec_lines.append('%autoreload 2')
 # c.InteractiveShellApp.exec_lines.append('%matplotlib osx')
 c.InteractiveShellApp.exec_lines.append('print("Warning: disable autoreload in ipython_config.py to improve performance.")')
+
+c.InteractiveShell.editor = 'vim'
