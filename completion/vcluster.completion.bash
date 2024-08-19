fec21770808c95c7836ddfe8af5c2185bfc3369b
Tiago Santos <tiago.dos.santos@hitachivantara.com>
2024-08-19 09:36:16 +0100

    remove unused completion

--- completion/vcluster.completion.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ ! -f $BASH_COMPLETION_USER_DIR/completions/vcluster ]] && is_app_installed vcluster; then
-  vcluster completion bash > $BASH_COMPLETION_USER_DIR/completions/vcluster
-fi
