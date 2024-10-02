3acd856a8b62ec54c3196b48e6cac495ed64de76
Tiago Santos <tiago.dos.santos@hitachivantara.com>
2024-10-02 09:24:01 +0100

    chore: rust

--- nvim/lua/plugins/rustaceanvim.lua
+++ nvim/lua/plugins/rustaceanvim.lua
@@ -1,11 +1,12 @@
 return {
   "mrcjkb/rustaceanvim",
   version = "^5", -- Recommended
-  lazy = false,   -- This plugin is already lazy
+  lazy = false, -- This plugin is already lazy
   init = function()
     vim.g.rustaceanvim = {
       -- Plugin configuration
       tools = {
+        enable_clippy = false,
         float_win_config = {
           border = "rounded",
         },
@@ -13,14 +14,30 @@ return {
       server = {
         default_settings = {
           -- rust-analyzer language server configuration
-          ['rust-analyzer'] = {
+          ["rust-analyzer"] = {
+            -- numThreads = 8,
+            cargo = {
+              allFeatures = true,
+              -- extraArgs = { "--jobs", "8" },
+            },
+            files = {
+              excludeDirs = {
+                ".cargo",
+                ".direnv",
+                ".git",
+                "node_modules",
+                "target",
+              },
+            },
             checkOnSave = false,
-            check = {
-              command = "clippy"
-            }
-          }
-        }
-      }
+            -- check = {
+            --   command = "clippy",
+            --   workspace = false,
+            --   -- features = "all",
+            -- },
+          },
+        },
+      },
     }
   end,
 }
