ab4c77c2db66d06b6439ac5d437d8f6b6b201c9e
Tiago Santos <t.santos84@gmail.com>
2024-12-21 11:58:11 +0000

    misc

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -6,6 +6,14 @@ end
 return {
   -- gruvbox
   {
+    "luisiacc/gruvbox-baby",
+    priority = 1000,
+    config = function()
+      vim.g.gruvbox_baby_background_color = "dark"
+      -- vim.cmd.colorscheme "gruvbox-baby"
+    end
+  },
+  {
     "ellisonleao/gruvbox.nvim",
     priority = 1000,
     -- lazy = false,
@@ -23,72 +31,72 @@ return {
     end,
   },
   -- rosé pine
-  {
-    "rose-pine/neovim",
-    priority = 1000,
-    name = "rose-pine",
-    -- lazy = true,
-    config = function()
-      require("rose-pine").setup({
-        disable_background = false,
-      })
-      -- vim.cmd.colorscheme "rose-pine-moon"
-    end
-  },
-  -- catppuccin
-  {
-    "catppuccin/nvim",
-    -- lazy = true,
-    name = "catppuccin",
-    priority = 1000,
-    opts = {
-      flavour = "macchiato", -- latte, frappe, macchiato, mocha
-      -- styles = {
-      --   keywords = { "bold" },
-      --   functions = { "italic" },
-      -- },
-      -- transparent_background = true,
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        harpoon = true,
-        mason = true,
-        native_lsp = {
-          enabled = true,
-          underlines = {
-            errors = { "undercurl" },
-            hints = { "undercurl" },
-            warnings = { "undercurl" },
-            information = { "undercurl" },
-          },
-        },
-        neotree = true,
-        -- noice = true,
-        lsp_trouble = true,
-        telescope = true,
-        treesitter = true,
-        treesitter_context = true,
-      },
-    },
-    config = function(_, opts)
-      require("catppuccin").setup(opts)
-      -- vim.cmd.colorscheme "catppuccin"
-    end
-  },
-
-  -- tokyonight
-  {
-    "folke/tokyonight.nvim",
-    -- lazy = true,
-    priority = 1000,
-    opts = {
-      style = "moon",
-      -- transparent = true,
-      lualine_bold = true,
-    },
-    config = function(_, opts)
-      require("tokyonight").setup(opts)
-      -- vim.cmd.colorscheme "tokyonight"
-    end
-  },
+  -- {
+  --   "rose-pine/neovim",
+  --   priority = 1000,
+  --   name = "rose-pine",
+  --   -- lazy = true,
+  --   config = function()
+  --     require("rose-pine").setup({
+  --       disable_background = false,
+  --     })
+  --     -- vim.cmd.colorscheme "rose-pine-moon"
+  --   end
+  -- },
+  -- -- catppuccin
+  -- {
+  --   "catppuccin/nvim",
+  --   -- lazy = true,
+  --   name = "catppuccin",
+  --   priority = 1000,
+  --   opts = {
+  --     flavour = "macchiato", -- latte, frappe, macchiato, mocha
+  --     -- styles = {
+  --     --   keywords = { "bold" },
+  --     --   functions = { "italic" },
+  --     -- },
+  --     -- transparent_background = true,
+  --     integrations = {
+  --       cmp = true,
+  --       gitsigns = true,
+  --       harpoon = true,
+  --       mason = true,
+  --       native_lsp = {
+  --         enabled = true,
+  --         underlines = {
+  --           errors = { "undercurl" },
+  --           hints = { "undercurl" },
+  --           warnings = { "undercurl" },
+  --           information = { "undercurl" },
+  --         },
+  --       },
+  --       neotree = true,
+  --       -- noice = true,
+  --       lsp_trouble = true,
+  --       telescope = true,
+  --       treesitter = true,
+  --       treesitter_context = true,
+  --     },
+  --   },
+  --   config = function(_, opts)
+  --     require("catppuccin").setup(opts)
+  --     -- vim.cmd.colorscheme "catppuccin"
+  --   end
+  -- },
+  --
+  -- -- tokyonight
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   -- lazy = true,
+  --   priority = 1000,
+  --   opts = {
+  --     style = "moon",
+  --     -- transparent = true,
+  --     lualine_bold = true,
+  --   },
+  --   config = function(_, opts)
+  --     require("tokyonight").setup(opts)
+  --     -- vim.cmd.colorscheme "tokyonight"
+  --   end
+  -- },
 }
