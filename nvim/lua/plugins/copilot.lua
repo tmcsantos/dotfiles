96e64a4804f0c2fb955676406267738db93c39e2
Tiago Santos <t.santos84@gmail.com>
2025-04-30 19:15:35 +0100

    misc

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,32 +1,69 @@
 return {
-  -- consider replacing with zbirenbaum/copilot.lua
-  "github/copilot.vim",
-  enabled = false,
-  config = function()
-    local opts = { remap = false, silent = true, noremap = true }
-
-    vim.g.copilot_assume_mapped = true
-    vim.g.copilot_no_tab_map = true
-    vim.g.copilot_filetypes = {
-      ["*"] = false,
-      ["javascript"] = true,
-      ["typescript"] = true,
-      ["lua"] = false,
-      ["rust"] = true,
-      ["c"] = true,
-      ["c#"] = true,
-      ["c++"] = true,
-      ["go"] = true,
-      ["python"] = true,
-    }
-
-    vim.g.copilot_enabled = false
-    vim.keymap.set("i", "<C-j>", '<C-R>=copilot#Accept("")<CR>', opts)
-    vim.keymap.set("i", "<C-l>", '<C-R>=copilot#Next()<CR>', opts)
-    vim.keymap.set("i", "<C-h>", '<C-R>=copilot#Previous()<CR>', opts)
-
-    function CopilotEnable()
-      vim.g.copilot_enabled = true
+  {
+    "olimorris/codecompanion.nvim",
+    opts = {},
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      local companion = require("codecompanion")
+      local adapters = require("codecompanion.adapters")
+      companion.setup({
+        strategies = {
+          chat = { adapter = "ollama_chat" },
+          inline = { adapter = "ollama_code" },
+        },
+        adapters = {
+          ollama_chat = function()
+            return adapters.extend("ollama", {
+              schema = {
+                model = { default = "cogito:3b" }
+              }
+            })
+          end,
+          ollama_code = function()
+            return adapters.extend("ollama", {
+              schema = {
+                model = { default = "qwen2.5-coder:1.5b" }
+              }
+            })
+          end
+        }
+      })
     end
-  end
+  },
 }
+
+-- return {
+--   -- consider replacing with zbirenbaum/copilot.lua
+--   "github/copilot.vim",
+--   enabled = false,
+--   config = function()
+--     local opts = { remap = false, silent = true, noremap = true }
+--
+--     vim.g.copilot_assume_mapped = true
+--     vim.g.copilot_no_tab_map = true
+--     vim.g.copilot_filetypes = {
+--       ["*"] = false,
+--       ["javascript"] = true,
+--       ["typescript"] = true,
+--       ["lua"] = false,
+--       ["rust"] = true,
+--       ["c"] = true,
+--       ["c#"] = true,
+--       ["c++"] = true,
+--       ["go"] = true,
+--       ["python"] = true,
+--     }
+--
+--     vim.g.copilot_enabled = false
+--     vim.keymap.set("i", "<C-j>", '<C-R>=copilot#Accept("")<CR>', opts)
+--     vim.keymap.set("i", "<C-l>", '<C-R>=copilot#Next()<CR>', opts)
+--     vim.keymap.set("i", "<C-h>", '<C-R>=copilot#Previous()<CR>', opts)
+--
+--     function CopilotEnable()
+--       vim.g.copilot_enabled = true
+--     end
+--   end
+-- }
