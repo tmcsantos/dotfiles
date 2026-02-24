748671e10342bb9f4cb2130e01c5a61d8c075322
Tiago Santos <t.santos84@gmail.com>
2026-02-24 14:27:25 +0000

    feat(nvim): updated treesitter configs

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,126 +1,157 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    lazy = false,
     build = ":TSUpdate",
     config = function()
-      local configs = require("nvim-treesitter.configs")
-      -- local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-      -- parser_config.wit = {
-      --   install_info = {
-      --     url = "https://github.com/Michael-F-Bryan/wit-lsp", -- local path or git repo
-      --     files = {                                           -- note that some parsers also require src/scanner.c or src/scanner.cc
-      --       "tree-sitter-wit/src/parser.c",
-      --     },
-      --     -- optional entries:
-      --     branch = "main",                        -- default branch in case of git repo if different from master
-      --     generate_requires_npm = false,          -- if stand-alone parser without npn dependencies
-      --     requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
-      --   },
-      --   filetype = "wit",                         -- if filetype does not match the parser name
-      -- }
-
-      configs.setup({
-        ensure_installed = {
-          "bash",
-          "c",
-          "css",
-          "dockerfile",
-          "go",
-          "gomod",
-          "graphql",
-          "java",
-          "javascript",
-          "json",
-          "jsonc",
-          "latex",
-          "lua",
-          "markdown",
-          "markdown_inline",
-          "python",
-          "rust",
-          "tsx",
-          "typescript",
-          "vim",
-          "vimdoc",
-          "yaml",
-        },
-        sync_install = false, -- Install parsers synchronously (only applied to `ensure_installed`)
-        -- Automatically install missing parsers when entering buffer
-        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-        auto_install = true,
-
-        highlight = {
-          -- `false` will disable the whole extension
-          enable = true,
-
-          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-          -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-          -- Using this option may slow down your editor, and you may see some duplicate highlights.
-          -- Instead of true it can also be a list of languages
-          additional_vim_regex_highlighting = false,
-        },
-        indent = { enable = true },
-        refactor = {
-          highlight_definitions = { enable = true },
-          highlight_current_scope = { enable = false },
-        },
-        textobjects = {
-          select = {
-            enable = true,
-
-            -- Automatically jump forward to textobj, similar to targets.vim
-            lookahead = true,
-
-            keymaps = {
-              ["aa"] = "@parameter.outer",
-              ["ia"] = "@parameter.inner",
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = "@class.inner",
-            },
-          },
-
-          move = {
-            enable = true,
-            set_jumps = true, -- whether to set jumps in the jumplist
-            goto_next_start = {
-              ["]m"] = "@function.outer",
-              ["]]"] = { query = "@class.outer", desc = "Next class start" },
-              --
-              -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queires.
-              ["]o"] = "@loop.*",
-              -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
-              --
-              -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
-              -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
-              ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
-              ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
-            },
-            goto_next_end = {
-              ["]M"] = "@function.outer",
-              ["]["] = "@class.outer",
-            },
-            goto_previous_start = {
-              ["[m"] = "@function.outer",
-              ["[["] = "@class.outer",
-            },
-            goto_previous_end = {
-              ["[M"] = "@function.outer",
-              ["[]"] = "@class.outer",
-            },
-            -- Below will go to either the start or the end, whichever is closer.
-            -- Use if you want more granular movements
-            -- Make it even more gradual by adding multiple queries and regex.
-            goto_next = {
-              ["]d"] = "@conditional.outer",
-            },
-            goto_previous = {
-              ["[d"] = "@conditional.outer",
-            }
-          },
-        },
+      local ts = require("nvim-treesitter")
+      ts.setup({})
+      ts.install({
+        "bash",
+        "c",
+        "cpp",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "rust",
+        "vim",
+        "vimdoc",
+        "yaml",
       })
+      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "*" },
+        callback = function()
+          local filetype = vim.bo.filetype
+          if filetype and filetype ~= "" then
+            local success = pcall(function()
+              vim.treesitter.start()
+            end)
+            if not success then
+              return
+            end
+          end
+        end,
+      })
+      -- local configs = require("nvim-treesitter.configs")
+      -- -- local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+      -- -- parser_config.wit = {
+      -- --   install_info = {
+      -- --     url = "https://github.com/Michael-F-Bryan/wit-lsp", -- local path or git repo
+      -- --     files = {                                           -- note that some parsers also require src/scanner.c or src/scanner.cc
+      -- --       "tree-sitter-wit/src/parser.c",
+      -- --     },
+      -- --     -- optional entries:
+      -- --     branch = "main",                        -- default branch in case of git repo if different from master
+      -- --     generate_requires_npm = false,          -- if stand-alone parser without npn dependencies
+      -- --     requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+      -- --   },
+      -- --   filetype = "wit",                         -- if filetype does not match the parser name
+      -- -- }
+      --
+      -- configs.setup({
+      --   ensure_installed = {
+      --     "bash",
+      --     "c",
+      --     "css",
+      --     "dockerfile",
+      --     "go",
+      --     "gomod",
+      --     "graphql",
+      --     "java",
+      --     "javascript",
+      --     "json",
+      --     "jsonc",
+      --     "latex",
+      --     "lua",
+      --     "markdown",
+      --     "markdown_inline",
+      --     "python",
+      --     "rust",
+      --     "tsx",
+      --     "typescript",
+      --     "vim",
+      --     "vimdoc",
+      --     "yaml",
+      --   },
+      --   sync_install = false, -- Install parsers synchronously (only applied to `ensure_installed`)
+      --   -- Automatically install missing parsers when entering buffer
+      --   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+      --   auto_install = true,
+      --
+      --   highlight = {
+      --     -- `false` will disable the whole extension
+      --     enable = true,
+      --
+      --     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+      --     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+      --     -- Using this option may slow down your editor, and you may see some duplicate highlights.
+      --     -- Instead of true it can also be a list of languages
+      --     additional_vim_regex_highlighting = false,
+      --   },
+      --   indent = { enable = true },
+      --   refactor = {
+      --     highlight_definitions = { enable = true },
+      --     highlight_current_scope = { enable = false },
+      --   },
+      --   textobjects = {
+      --     select = {
+      --       enable = true,
+      --
+      --       -- Automatically jump forward to textobj, similar to targets.vim
+      --       lookahead = true,
+      --
+      --       keymaps = {
+      --         ["aa"] = "@parameter.outer",
+      --         ["ia"] = "@parameter.inner",
+      --         ["af"] = "@function.outer",
+      --         ["if"] = "@function.inner",
+      --         ["ac"] = "@class.outer",
+      --         ["ic"] = "@class.inner",
+      --       },
+      --     },
+      --
+      --     move = {
+      --       enable = true,
+      --       set_jumps = true, -- whether to set jumps in the jumplist
+      --       goto_next_start = {
+      --         ["]m"] = "@function.outer",
+      --         ["]]"] = { query = "@class.outer", desc = "Next class start" },
+      --         --
+      --         -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queires.
+      --         ["]o"] = "@loop.*",
+      --         -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
+      --         --
+      --         -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
+      --         -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
+      --         ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
+      --         ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+      --       },
+      --       goto_next_end = {
+      --         ["]M"] = "@function.outer",
+      --         ["]["] = "@class.outer",
+      --       },
+      --       goto_previous_start = {
+      --         ["[m"] = "@function.outer",
+      --         ["[["] = "@class.outer",
+      --       },
+      --       goto_previous_end = {
+      --         ["[M"] = "@function.outer",
+      --         ["[]"] = "@class.outer",
+      --       },
+      --       -- Below will go to either the start or the end, whichever is closer.
+      --       -- Use if you want more granular movements
+      --       -- Make it even more gradual by adding multiple queries and regex.
+      --       goto_next = {
+      --         ["]d"] = "@conditional.outer",
+      --       },
+      --       goto_previous = {
+      --         ["[d"] = "@conditional.outer",
+      --       }
+      --     },
+      --   },
+      -- })
     end
   },
   { 'nvim-treesitter/nvim-treesitter-context' },
