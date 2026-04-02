a8d70936581acaff3f0e5495534ddaa597c6bc2d
Tiago Santos <t.santos84@gmail.com>
2026-04-02 12:15:48 +0100

    feat: add script to rebind new ssh agent socket

--- home/dot_profile.d/ssh-agent.sh
+++ home/dot_profile.d/ssh-agent.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# setup gpg-agent ssh
-# launch gpg-agent if not up
-if ! pgrep -u "$USER" gpg-agent >/dev/null; then
-  eval "$(gpg-agent --daemon)"
+if ! pgrep --list-full --uid "$USER" --exact ssh-agent >/dev/null; then
+  eval "$(ssh-agent)"
 fi
