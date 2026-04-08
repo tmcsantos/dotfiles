a340d4429014dd3d03cf2dd587b9f13c6b885f66
Tiago Santos <t.santos84@gmail.com>
2026-04-08 20:15:07 +0100

    feat: macos support

--- home/dot_profile.d/ssh-agent.sh
+++ home/dot_profile.d/ssh-agent.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-if ! pgrep --list-full --uid "$USER" --exact ssh-agent >/dev/null; then
+if ! pgrep -U "$USER" ssh-agent >/dev/null; then
   eval "$(ssh-agent)"
 fi
--- nvim/init.lua
+++ nvim/init.lua
@@ -2,25 +2,8 @@
 -- if you have any mappings you set BEFORE doing this, they will be set to the OLD
 -- leader.
 vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
 
+require("config.lazy")
 require("config")
-require("lazy").setup({
-  spec = "plugins",
-  change_detection = { notify = false }
-  -- ui = {
-  --   border = "rounded",
-  -- },
-})
